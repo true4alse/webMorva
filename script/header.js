51e34f36c4b2453f5bb2dd5936394b8662d55175
정현진 <green4alse@gmail.com>
2026-03-12 16:09:47 +0900

    jhjhjhj

---
+++ script/header.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    // 메뉴버튼을 누르면 header가 나오는 기능
+    const btnMenu = document.querySelector('.btn-menu');
+    const menuSmartHidden = document.querySelector('.menu-smart-hidden');
+    const btnClose = document.querySelector(".btn-close")
+    const gnbSmartList = document.querySelectorAll(".gnb-smart>li")
+
+    btnMenu.addEventListener('click', () => {
+        menuSmartHidden.classList.add('on');
+    });
+    btnClose.addEventListener("click",()=>{
+        menuSmartHidden.classList.remove('on');
+        gnbSmartList.forEach(tag=>tag.classList.remove("on"))
+    })
+
+
+    // 모바일에서 2뎁스 메뉴 나오는 기능
+    const btnMores = document.querySelectorAll(".gnb-smart li .btn-more")
+    btnMores.forEach(span=>{
+        span.addEventListener('click',()=>{
+            span.parentElement.classList.toggle("on")
+        })
+    })
+
+})
