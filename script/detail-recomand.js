218ac2a9585228563589cfc3105819cd1e1011b7
정현진 <green4alse@gmail.com>
2026-03-25 15:02:51 +0900

    detail finished css

---
+++ script/detail-recomand.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    // ul태그에 li태그의 형태로 상품 데이터를 넣는 소스코드
+    const list = document.querySelector(".list-grid")
+    let result = ""
+    listArray.forEach(item=>{
+        result += `
+        <li>
+            <a href="#">
+                <figure>
+                    <img src="./img/${item.pThumbnail}" alt="${item.pName}">
+                </figure>
+                <div class="list-option">
+                    <span class="btn-heart">
+                        <img src="./img/icn-heart.svg" alt="좋아요 아이콘">
+                    </span>
+                </div>
+                <h4>${item.pName}</h4>
+                <p class="fwm">${item.pDesc}</p>
+                <div class="price fwb">${toWon(item.pPrice)}</div>
+            </a>
+        </li>`
+    })
+    list.innerHTML = result
+
+})
